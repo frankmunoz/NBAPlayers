45262b4dbdae8a6960cfb662d0550b49e536e0dd
Frank Munoz <ingeniero.frank@gmail.com>
2022-02-06 20:41:21 -0500

    Added SSL Certified to solve conflicts with URL

--- README.md
+++ README.md
@@ -7,10 +7,10 @@ basado en la entrada del usuario.  Los datos originalmente vienen de
 [aquí](https://www.openintro.org/data/index.php?data=nba_heights).  Los datos
 son servidos en formato json [aquí](https://mach-eight.uc.r.appspot.com/).
 
-La tarea es crear una aplicación que solicita al usario que ingrese una entrada
+La tarea es crear una aplicación que solicita al usuario que ingrese una entrada
 numérica. La aplicación descarga los datos del sitio web arriba
 (https://mach-eight.uc.r.appspot.com) e imprime todos los pares de jugadores
-cuyas alturas en pulgadas se suman al número de entrada. Si no se encuentra
+cuyas alturas en pulgadas se suman al número de entrada. Si no se encuentran
 coincidentes, la aplicación imprime "No coincidentes encontrada"
 
 Ejemplo de salida de la siguiente manera:
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from urllib.request import urlopen
 import json
 import numpy as np
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 URL = "https://mach-eight.uc.r.appspot.com/"
 
 
 def clean_data(data):
-    number = 139
-
     a = {}
     for i in data["values"]:
         if i["h_in"] not in a:
@@ -35,24 +35,25 @@ def order_players(data, number):
     # remaining height, this is a problem of combinations without repetitions taking this into account doesn't really
     # matter if we have (a,b) or (b,a), we use the couple_of_players dictionary to set an id to each combination and
     # not repeat it
+    if data != {}:
+        for key in data:
+            choice = number - int(key)
+            if str(choice) in data and f'{key}-{choice}' not in couple_of_players and f'{choice}-{key}' not in couple_of_players:
+                couple_of_players[f'{key}-{choice}'] = [data[str(choice)], data[key]]
 
-    for key in data:
-        choice = number - int(key)
-        if str(choice) in data and f'{key}-{choice}' not in couple_of_players and f'{choice}-{key}' not in couple_of_players:
-            couple_of_players[f'{key}-{choice}'] = [data[str(choice)], data[key]]
+    if couple_of_players == {}:
+        print("No coincidentes encontrada")
+    else:
+        # we use numpy to get all the combinations in an "optimal" way
+        for key in couple_of_players:
+            mesh = np.array(np.meshgrid(couple_of_players[key][0], couple_of_players[key][1]))
+            combinations = mesh.T.reshape(-1, 2)
 
-    print(couple_of_players)
-
-    # we use numpy to get all the combinations in an "optimal" way
-    for key in couple_of_players:
-        mesh = np.array(np.meshgrid(couple_of_players[key][0], couple_of_players[key][1]))
-        combinations = mesh.T.reshape(-1, 2)
-
-        print('\n'.join('- {}         {}'.format(*k) for k in combinations))
+            print('\n'.join('- {}         {}'.format(*k) for k in combinations))
 
 
 if __name__ == '__main__':
-    number = int(input())
+    m_number = int(input())
     m_data = get_data(URL)
     m_data = clean_data(m_data)
-    order_players(m_data, number)
+    order_players(m_data, m_number)
